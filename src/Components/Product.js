b9fc02e5033accfaf38cf1ad36e5089a4a90a9b9
Haris Rajput <haris.rajput@visionetsystems.com>
2021-10-24 15:47:12 +0500

    jquery added

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,8 +1,11 @@
 import { useEffect, useState } from 'react'
-import style from './styles/product.module.scss'
+import $ from 'jquery';
+import './styles/product.scss'
+
 const Product = ({ data }) => {
     const [productImage, setProductImage] = useState(null);
     const [productColors, setProductColors] = useState([]);
+
     useEffect(() => {
         console.log('data', data)
         if (data) {
@@ -10,35 +13,81 @@ const Product = ({ data }) => {
             setProductColors(data.sizes[0].colors)
         }
     })
+
+
+    $('.sizesButtons').on('click', 'button', (e) => {
+        $('.sizesButtons').children().removeClass('selectedSize')
+        $(e.target).addClass('selectedSize');
+        console.log('$(e.target).val()', $(e.target).val())
+        $("#selectedSize").val($(e.target).val())
+    })
+
+    $('.colorsButtons').on('click', 'button', (e) => {
+        $('.colorsButtons').children().removeClass('selectedColor')
+        $(e.target).addClass('selectedColor');
+        let selectedImage = $(e.target).next('input[type=hidden]').val();
+        $(".p-image img").attr("src", selectedImage);
+        $("#selectedColor").val($(e.target).val())
+    })
+
+    $('.addToCart').on('click', (e) => {
+        let selectedSize = $("#selectedSize").val();
+        let selectedColor = $("#selectedColor").val();
+        $('.alert').removeClass('alert-danger')
+        $('.alert').removeClass('alert-success')
+        $('.alert').text('')
+        if (!selectedSize) {
+            $('.alert').addClass('alert-danger')
+            $('.alert').text('Please select size')
+            return;
+        }
+        if (!selectedColor) {
+            $('.alert').addClass('alert-danger')
+            $('.alert').text('Please select color')
+            return;
+        }
+        $('.alert').addClass('alert-success')
+        $('.alert').text('Item Added')
+    })
+
+
     if (!data) {
         return null;
     }
     return (
         <div className="row">
-            <section className={`col-md ${style.productImageWrapper}`}>
+            <section className={`col-md p-image productImageWrapper`}>
                 <img src={productImage} alt={data.title} />
             </section>
-            <section className={`col-md-8 ${style.productDetailWrapper}`}>
+            <section className={`col-md-8 productDetailWrapper`}>
                 <h2>{data.title}</h2>
                 <h3 className="note">AED {data.price}</h3>
-                <div className={style.availableSizes}>
+                <div className="availableSizes">
                     <h4>Available Sizes:</h4>
-                    <ul>
+                    <div className="sizesButtons">
                         {data.sizes && data.sizes.map((x, i) =>
-                            <li><button className="btn btn-link" value={x.value}>{x.displayName}</button></li>
+                            <>
+                                <button className="btn btn-link" value={x.value}>{x.displayName}</button>
+                            </>
                         )}
-                    </ul>
-                </div>
-                <div className={style.availableColors}>
-                    <h4>Colors</h4>
-                    <div>
-                        {productColors && productColors.map((x, i) =>
-                            <button className={`btn ${x.selecetd ? style.selectedColor : ''}`} style={{ backgroundColor: x.value }} value={x.value}>&nbsp;</button>
-                        )}
+                        <input type="hidden" id="selectedSize" />
                     </div>
                 </div>
-                <button className={"btn btn-primary"}>Add To Cart</button>
-                <div className={style.description}>
+                <div className="availableColors">
+                    <h4>Colors</h4>
+                    <div className="colorsButtons">
+                        {productColors && productColors.map((x, i) =>
+                            <>
+                                <button className={`btn color-${x.id}`} style={{ backgroundColor: x.value }} value={x.id}>&nbsp;</button>
+                                <input type="hidden" value={x.imageUrl} />
+                            </>
+                        )}
+                        <input type="hidden" id="selectedColor" />
+                    </div>
+                </div>
+                <button type="submit" className="addToCart btn btn-primary">Add To Cart</button>
+                <div className="alert mt-3"></div>
+                <div className="description">
                     <h4>Description</h4>
                     <p>{data.description}</p>
                 </div>
